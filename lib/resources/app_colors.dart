85c05ac0d10439d6fc644790ff473ceb40a8e235
Toru Choi <blueocean239@gmail.com>
2025-09-21 23:44:30 +0900

    feat: define string and color resources

---
+++ lib/resources/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  const AppColors._();
+
+  static const Color primary = Color(0xFF5B7BFF);
+  static const Color gradientTop = Color(0xFFF1F4FF);
+  static const Color gradientBottom = Color(0xFFFFFFFF);
+}
