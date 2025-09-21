3f777451332a971ca37e166ab010e5ecf46d88a1
Toru Choi <blueocean239@gmail.com>
2025-09-22 00:23:52 +0900

    feat: added width to be defined out of appcard

--- lib/widgets/app_card.dart
+++ lib/widgets/app_card.dart
@@ -7,19 +7,21 @@ class AppCard extends StatelessWidget {
     this.padding = const EdgeInsets.all(20),
     this.margin = EdgeInsets.zero,
     this.elevation = 2,
+    this.width = double.infinity,
   });
 
   final Widget child;
   final EdgeInsetsGeometry padding;
   final EdgeInsetsGeometry margin;
   final double elevation;
+  final double width;
 
   @override
   Widget build(BuildContext context) {
     final radius = BorderRadius.circular(16);
 
     return SizedBox(
-      width: double.infinity,
+      width: width,
       child: Card(
         margin: margin,
         elevation: elevation,
